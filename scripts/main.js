9e3ff529048d21ef000d9e7981d98a338c6592ab
Sean Obringer <SeanObringer90@gmail.com>
2017-09-19 10:29:54 -0700

    added event listeners

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,26 @@ console.log(cells)
 
 
 for(let i =0; i<cells.length; i++){
-cells[i].addEventListener('click', function(event){
+  cells[i].addEventListener('click', function(event){
 
-  event.target.style.color='tomato'
-
+    event.target.style.backgroundColor='tomato'
+    //console.log(cells[i])
 
 })
+}
+
+var brushColor = document.getElementsByClassName('swatch')
+
+for(let x = 0; x<brushColor.length; x++){
+  brushColor[x].addEventListener('click', function(event){
+
+    event.target.
+
+
+
+  })
+
+
+
+
 }
